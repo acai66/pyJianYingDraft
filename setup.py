19cbcc41a6fdd077c574180972060f67002d4612
acai <1779864536@qq.com>
2025-07-10 15:17:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.5",
     author="gary318",
     description="轻量、灵活、易上手的Python剪映草稿生成及导出工具，构建全自动化视频剪辑/混剪流水线",
-    long_description=open("pypi_readme.md", "r", encoding="utf-8").read(),
+    long_description="Fork from https://github.com/GuanYixuan/pyJianYingDraft",
     long_description_content_type="text/markdown",
     url="https://github.com/GuanYixuan/pyJianYingDraft",
     packages=find_packages(),
@@ -15,13 +15,13 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
-        "Operating System :: Microsoft :: Windows",
+        # "Operating System :: Microsoft :: Windows",
         "Topic :: Multimedia :: Video"
     ],
     python_requires='>=3.8',
     install_requires=[
         "pymediainfo",
         "imageio",
-        "uiautomation>=2"
+        # "uiautomation>=2"
     ],
 )
